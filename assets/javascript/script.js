f23af06432677088b33a04d7ac76d4c5a0631b73
JLasc <lascaze.j@gmail.com>
2018-07-27 19:34:23 -0400

    add  firebase initializer

---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB7nHRA8mThsS7JoZOJ_mKIcKTeMaDo7Ls",
+    authDomain: "train-scheduler-8c05a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-8c05a.firebaseio.com",
+    projectId: "train-scheduler-8c05a",
+    storageBucket: "train-scheduler-8c05a.appspot.com",
+    messagingSenderId: "962906673522"
+  };
+  firebase.initializeApp(config);
+
+  var databse = firebase.database();
+
+  
+});
